52b95a0d75dd1ffdfd33a3aebba3b601679e3b99
M. Coleman <nc2jax@hotmail.com>
2020-07-01 22:50:45 -0400

    updating package.json file in smart-brain-api repository

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const profile = require('./Controllers/profile');
+const image = require('./Controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+//How to create an Array in node.js (array of database users)
+
+
+app.get('/', (req, res) => {
+    res.json(database.users);
+})
+
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
+
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
+
+app.put('/image', (req,res) => {image.handleImage(req,res,db)})
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
+
+app.listen(process.env.PORT || 3001, () =>{
+    console.log(`app is running on port ${process.env.PORT}`);
+})
